11c12fdf0ced4a19b22c757d45a2f39a376b3c70
Araz Nabi <araznabi@gmail.com>
2023-10-17 14:15:43 +0200

    small fixes

--- create_png.py
+++ create_png.py
@@ -3,8 +3,41 @@ import matplotlib.pyplot as plt
 
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-# Load the image
-diagram_image = plt.imread(CURR_DIR_PATH + '\\empty_sign.png')
-plt.text(x=80, y=105, s="ABX 123", fontsize=70)
-plt.imshow(diagram_image)
-plt.show()
+
+
+reg_list = ['APA 123','BAJ 554','BSS 532','FUK 065','CUK 275','SEX 923','FAN 368','PAJ 960','GAM 9311','GYN','HAT',\
+            'ZUP 612','HKH','TYP 023','UFO 532','USA 064','WAM 853','XTC','XTZ 041','ZEX','KKK 638','ZUG','ZOO']
+
+def generate_images_and_save(characters_list, output_directory):
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for random_plate in characters_list:
+        # Get the first character to determine the folder name
+        first_character = random_plate[0]
+
+        # Create a folder for the category if it doesn't exist
+        category_directory = os.path.join(output_directory, first_character)
+        if not os.path.exists(category_directory):
+            os.makedirs(category_directory)
+
+        for _ in range(len(characters_list)):
+            # Create an image with the registration plate
+            plate_image = plt.imread(CURR_DIR_PATH + '\\empty_sign.png')
+            plt.text(x=80, y=105, s=random_plate, fontsize=70)
+
+            # Save the image to the category folder
+            image_filename = f"{random_plate}.png"
+            image_path = os.path.join(category_directory, image_filename)
+            plt.imshow(plate_image)
+            plt.savefig(image_path)
+            plt.close()
+
+# Example usage:
+characters_list = reg_list
+output_directory = (CURR_DIR_PATH + '\\registration_images')
+
+generate_images_and_save(characters_list, output_directory)
+
+print("Images generated and saved.")
