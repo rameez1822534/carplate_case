42c371f50d5eb7053a2aae140321992f92dc3ee1
Araz Nabi <araznabi@gmail.com>
2023-10-17 14:35:34 +0200

    latest fixes

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 reg_list = create_plate(3)
 output_directory = (CURR_DIR_PATH + '\\registration_images')
 generate_images_and_save(reg_list, output_directory)
-file_type = '.png'
-def find_files(file_type):
-    """Use filetype to count them in folder screenshots"""
-    file_count = 0
-    for filename in os.listdir(output_directory):
-        if filename.lower().endswith(file_type):
-            file_count += 1
 
-    if file_count >= 1:
-        print(f"the folder has {file_count} {file_type} files")
-    else:
-        print(f"There are no {file_type} files in this folder")
+
+# file_type = '.png'
+# def find_files(file_type):
+#     """Use filetype to count them in folder screenshots"""
+#     file_count = 0
+#     for filename in os.listdir(output_directory):
+#         if filename.lower().endswith(file_type):
+#             file_count += 1
+
+#     if file_count >= 1:
+#         print(f"the folder has {file_count} {file_type} files")
+#     else:
+#         print(f"There are no {file_type} files in this folder")
+
+# find_files(file_type)
